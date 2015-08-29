688067238333aa6d643101866a34ea37979d307b
Saurabh <singhalsaurabh95@gmail.com>
2015-08-29 20:30:53 +0530

    Added camera move and markers on suggestion touch. Updated build tools.

--- app/src/main/java/com/xpdire/easeyourshop/MainActivity.java
+++ app/src/main/java/com/xpdire/easeyourshop/MainActivity.java
@@ -1,22 +1,33 @@
 package com.xpdire.easeyourshop;
 
+import android.content.res.Resources;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -25,6 +36,58 @@ public class MainActivity extends ActionBarActivity implements TouchableWrapper.
     Location lastlocation;
     Toolbar maptoolbar;
     private GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter placeAutocompleteAdapter;
+    private Marker lastmarker;
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e("ass", "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+            //Remove Old Marker.
+            if (lastmarker!=null){
+                lastmarker.remove();
+            }
+
+            // Format details of the place for display and show it in  the Map.
+//            Check For Null bounds.
+            if (place.getViewport()!=null){
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(place.getViewport(),400,400,0/*Set desired Padding*/);
+                lastmarker=map.addMarker(new MarkerOptions().title(place.getName().toString()).snippet(place.getAddress().toString()).position(place.getLatLng()));
+                map.animateCamera(cameraUpdate);
+            }
+            else if (place.getLatLng()!=null){
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(place.getLatLng(),15);
+                lastmarker=map.addMarker(new MarkerOptions().title(place.getName().toString()).snippet(place.getAddress().toString()).position(place.getLatLng()));
+                map.animateCamera(cameraUpdate);
+            }
+
+
+
+            // Display the third party attributions if set.
+
+
+            Log.i("ass", "Place details received: " + place.getName());
+
+            places.release();
+        }
+    };
+
+    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+        Log.e("ass", res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +107,27 @@ public class MainActivity extends ActionBarActivity implements TouchableWrapper.
         map.getUiSettings().setZoomControlsEnabled(true);
         final AutoCompleteTextView suggestions = (AutoCompleteTextView) findViewById(R.id.suggestions);
         final LatLngBounds WHOLE_EARTH = new LatLngBounds(new LatLng(-84.9, -180), new LatLng(84.9, 180));
-        suggestions.setAdapter(new PlaceAutocompleteAdapter(this, android.R.layout.simple_list_item_1, mGoogleApiClient, WHOLE_EARTH, null));
+        suggestions.setAdapter(placeAutocompleteAdapter=new PlaceAutocompleteAdapter(this, android.R.layout.simple_list_item_1, mGoogleApiClient, WHOLE_EARTH, null));
+        suggestions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               final PlaceAutocompleteAdapter.PlaceAutocomplete item = placeAutocompleteAdapter.getItem(position);
+                /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+              details about the place.
+              */
+                PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                        .getPlaceById(mGoogleApiClient, item.placeId.toString());
+                placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+            }
+        });
         map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
                 lastlocation = location;
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                 CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
-                map.addMarker(new MarkerOptions().title("Its Me!").snippet("My current location.").position(latLng));
+                lastmarker=map.addMarker(new MarkerOptions().title("Its Me!").snippet("My current location.").position(latLng));
                 map.animateCamera(cameraUpdate);
                 map.setOnMyLocationChangeListener(null);
             }
